a3c9cb3463798865bc2c5c94392fa3b230715508
ribeirom <rsilva.maycon@gmail.com>
2021-07-30 08:21:11 -0300

    desafio node com mysql e proxy reverso nginx

--- docker/node/index.js
+++ docker/node/index.js
@@ -7,18 +7,48 @@ const config = {
     password: 'root',
     database: 'nodedb'
 };
+
+//var engines = require('consolidate');
+//app.engine('html', require('ejs'));
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
+
+var path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Maycon')`
-connection.query(sql)
-connection.end()
+var Moniker = require('moniker');
+var names = Moniker.generator([Moniker.adjective, Moniker.noun]);
+var name = names.choose();
 
+const sql = `INSERT INTO people(name) values('` + name + `')`
+connection.query(sql)
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle</h1>')
+    connection.query("SELECT name FROM people", (err, result, fields) => {        
+        if(err) {
+            console.log(err); 
+            res.json({"error":true});
+        }
+        else { 
+            console.log(result);
+            /*if(result.length> 0){
+                for(let index = 0; index < result.length; index++) {
+                    var newRow = $("<tr>");
+                    var cols = "";
+                    cols += '<td> '+ result.rows[index].name +'</td>';
+                    newRow.append(cols);
+                    $("#tableData .tbody").append(newRow);
+                }
+            }*/
+            res.render(__dirname + '/views', { userData: result })
+        }
+    });
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
