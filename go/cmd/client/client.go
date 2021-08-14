f747f0c5b154a7285a799026fc9f6c7a190062c2
ribeirom <rsilva.maycon@gmail.com>
2021-08-14 16:31:26 -0300

    implementado streams

--- go/cmd/client/client.go
+++ go/cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/ribeirom/fullcycle/go/pb"
 	"google.golang.org/grpc"
@@ -17,7 +19,9 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	// AddUser(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -30,6 +34,77 @@ func AddUser(client pb.UserServiceClient) {
 	res, err := client.AddUser(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Joao",
+		Email: "j@j.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receuve the msg: %v", err)
+		}
+		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		{
+			Id:    "m1",
+			Name:  "Maycon",
+			Email: "maycon@maycon.com",
+		},
+		{
+			Id:    "m2",
+			Name:  "Maycon 2",
+			Email: "maycon@maycon.com",
+		},
+		{
+			Id:    "m3",
+			Name:  "Maycon 3",
+			Email: "maycon@maycon.com",
+		},
+		{
+			Id:    "m4",
+			Name:  "Maycon 4",
+			Email: "maycon@maycon.com",
+		},
+		{
+			Id:    "m5",
+			Name:  "Maycon 5",
+			Email: "maycon@maycon.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response %v", err)
 	}
 
 	fmt.Println(res)
